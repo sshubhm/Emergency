23be6f37c93cbba28ba8d98328bc669298278fc0
sshubhm <shubhamsapra@hotmail.com>
2017-01-27 14:23:26 +0530

    "Hy"

---
+++ app/src/main/java/com/falcondevs/emergency/MainActivity.java
@@ -0,0 +1,73 @@
+package com.falcondevs.emergency;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.location.LocationManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+    private double longitude;
+    private double latitude;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void gl(View view) {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText(longitude + " , " + latitude);
+
+    }
+}
---
+++ app/src/main/java/com/falcondevs/emergency/UserLocation.java
@@ -0,0 +1,12 @@
+package com.falcondevs.emergency;
+
+/**
+ * Created by shubham on 27/1/17.
+ */
+
+public class UserLocation {
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String address;
+}
